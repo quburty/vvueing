a2f732bd67ba11f264987d8a15124e2b446bd825
quburty <quburty@naver.com>
2023-09-26 21:24:30 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="modal" class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지내용임</p>
+      <button @click="modal = false">Close</button>
+    </div>
+  </div>
+  <img alt="Vue logo" src="./assets/logo.png" @click="modal = true" />
+  <div v-for="item in rooms" :key="item">
+    <h4>{{ item.name }}</h4>
+    <p>{{ item.price }}만원</p>
+    <button @click="item.report++"></button>
+    <p>신고수: {{ item.report }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import rooms from "./assets/data.js";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      price1: 50000,
+      price2: 30000,
+      products: ["역삼동원룸", "천호동원룸", "마포고원룸"],
+      reports: 0,
+      modal: false,
+      rooms: rooms,
+    };
+  },
+  methods: {
+    increase() {
+      this.reports++;
+    },
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -22,5 +47,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+button {
+  height: 30px;
 }
 </style>
---
+++ src/assets/data.js
@@ -0,0 +1,24 @@
+var rooms = [
+  {
+    name: "청담동원룸",
+    price: 1000,
+    report: 0,
+  },
+  {
+    name: "보문동원룸",
+    price: 2000,
+    report: 0,
+  },
+  {
+    name: "안암동원룸",
+    price: 3000,
+    report: 0,
+  },
+  {
+    name: "종로구원룸",
+    price: 4000,
+    report: 0,
+  },
+];
+
+export default rooms;
